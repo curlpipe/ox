75ac8554978d49a4a2bc66711cdfc3f06a12810b
Curly <11898833+curlpipe@users.noreply.github.com>
2020-09-24 09:39:29 +0100

    Changed default theme

--- src/config_default.rs
+++ src/config_default.rs
@@ -2,20 +2,20 @@
 use termion::color;
 
 // Set up background colours
-pub const BG: color::Bg<color::Rgb> = color::Bg(color::Rgb(33, 33, 48));
-pub const STATUS_BG: color::Bg<color::Rgb> = color::Bg(color::Rgb(51, 51, 72));
+pub const BG: color::Bg<color::Rgb> = color::Bg(color::Rgb(40, 42, 54));
+pub const STATUS_BG: color::Bg<color::Rgb> = color::Bg(color::Rgb(68, 71, 90));
 pub const RESET_BG: color::Bg<color::Reset> = color::Bg(color::Reset);
 
 // Set up foreground colours
 pub const FG: color::Fg<color::Rgb> = color::Fg(color::Rgb(255, 255, 255));
-pub const STATUS_FG: color::Fg<color::Rgb> = color::Fg(color::Rgb(35, 240, 144));
+pub const STATUS_FG: color::Fg<color::Rgb> = color::Fg(color::Rgb(80, 250, 123));
 pub const RESET_FG: color::Fg<color::Reset> = color::Fg(color::Reset);
 
 // For holding the tab width (how many spaces in a tab)
 pub const TAB_WIDTH: usize = 4;
 
 // Line numbers
-pub const LINE_NUMBER_FG: color::Fg<color::Rgb> = color::Fg(color::Rgb(48, 57, 102));
+pub const LINE_NUMBER_FG: color::Fg<color::Rgb> = color::Fg(color::Rgb(68, 71, 90));
 pub const LINE_NUMBER_PADDING: usize = 1;
 
 // Undo features
