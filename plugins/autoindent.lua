f5513d58b029c2241af4fae8eb5f4a3f7f1ec8e1
Luke <11898833+curlpipe@users.noreply.github.com>
2024-09-25 21:33:29 +0100

    Autoindent now dedents where appropriate

--- plugins/autoindent.lua
+++ plugins/autoindent.lua
@@ -1,5 +1,5 @@
 --[[
-Auto Indent v0.1
+Auto Indent v0.2
 
 You will be able to press return at the start of a block and have
 Ox automatically indent for you.
@@ -9,20 +9,21 @@ the character to the left of the cursor being an opening bracket or
 other syntax that indicates a block has started e.g. ":" in python
 ]]--
 
+-- Automatic Indentation
 event_mapping["enter"] = function()
     -- Get line the cursor was on
     y = editor.cursor.y - 1
     line = editor:get_line_at(y)
     -- Work out what the last character on the line was
     sline = line:gsub("^%s*(.-)%s*$", "%1")
+    local function starts(prefix)
+        return sline:sub(1, #prefix) == prefix
+	end
     local function ends(suffix) 
         return suffix == "" or sline:sub(-#suffix) == suffix 
     end
-    local function starts(prefix)
-        return sline:sub(1, #prefix) == prefix
-    end
     -- Work out how indented the line was
-    indents = #(line:match("^\t+") or "") + #(line:match("^ +") or "") / 4
+    indents = #(line:match("^\t+") or "") + #(line:match("^ +") or "") / document.tab_width
     -- Account for common groups of block starting characters
     is_bracket = ends("{") or ends("[") or ends("(")
     if is_bracket then indents = indents + 1 end
@@ -33,7 +34,7 @@ event_mapping["enter"] = function()
         if ends("do") then indents = indents + 1 end
     elseif editor.document_type == "Lua" then
         func = ends(")") and (starts("function") or starts("local function"))
-        if ends("do") or ends("then") or func then indents = indents + 1 end
+        if ends("else") or ends("do") or ends("then") or func then indents = indents + 1 end
     elseif editor.document_type == "Haskell" then
         if ends("where") or ends("let") or ends("do") then indents = indents + 1 end
     elseif editor.document_type == "Shell" then
@@ -45,6 +46,33 @@ event_mapping["enter"] = function()
     end
 end
 
--- Helper function to check string endings
-local function ends_with(str, ending)
+-- Automatic Dedenting
+local function do_dedent()
+	local current_line = editor:get_line()
+    if current_line:match("\t") ~= nil then
+        editor:insert_line_at(current_line:gsub("\t", "", 1), editor.cursor.y)
+        editor:remove_line_at(editor.cursor.y + 1)
+    else
+        editor:insert_line_at(current_line:gsub(string.rep(" ", document.tab_width), "", 1), editor.cursor.y)
+        editor:remove_line_at(editor.cursor.y + 1)
+    end
 end
+
+event_mapping["*"] = function()
+	line = editor:get_line()
+    local function ends(suffix) 
+        return line:match("^%s*" .. suffix .. "$") ~= nil
+    end
+	if editor.document_type == "Shell" then
+        if ends("fi") or ends("done") or ends("esac") or ends("}") or ends("elif") or ends("else") or ends(";;") then do_dedent() end
+	elseif editor.document_type == "Python" then
+		if ends("else") or ends("elif") or ends("except") or ends("finally") then do_dedent() end
+	elseif editor.document_type == "Ruby" then
+		if ends("end") or ends("else") or ends("elseif") or ends("ensure") or ends("rescue") or ends("when") or ends(";;") then do_dedent() end
+	elseif editor.document_type == "Lua" then
+		if ends("end") or ends("else") or ends("elseif") or ends("until") then do_dedent() end
+	elseif editor.document_type == "Haskell" then
+		if ends("else") or ends("in") then do_dedent() end
+    end
+end
+
