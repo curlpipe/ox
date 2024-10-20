fcf69800f40b0896d35f837558f5edcfe1c8c08a
Luke <11898833+curlpipe@users.noreply.github.com>
2024-10-20 13:57:01 +0100

    Complete reimplementation of word deletion and moving

--- kaolinite/src/document.rs
+++ kaolinite/src/document.rs
@@ -662,6 +662,38 @@ impl Document {
         self.cancel_selection();
     }
 
+    /// Find the word boundaries
+    pub fn word_boundaries(&mut self, line: &str) -> Vec<(usize, usize)> {
+        let re = r"(\s{2,}|[A-Za-z0-9_]+|\.)";
+        let mut searcher = Searcher::new(re);
+        let starts: Vec<Match> = searcher.lfinds(line);
+        let mut ends: Vec<Match> = starts.clone();
+        ends.iter_mut()
+            .for_each(|m| m.loc.x += m.text.chars().count());
+        let starts: Vec<usize> = starts.iter().map(|m| m.loc.x).collect();
+        let ends: Vec<usize> = ends.iter().map(|m| m.loc.x).collect();
+        starts.into_iter().zip(ends).collect()
+    }
+
+    /// Find the current state of the cursor in relation to words
+    pub fn cursor_word_state(&mut self, words: &[(usize, usize)], x: usize) -> WordState {
+        let in_word = words
+            .iter()
+            .position(|(start, end)| *start <= x && x <= *end);
+        if let Some(idx) = in_word {
+            let (word_start, word_end) = words[idx];
+            if x == word_end {
+                WordState::AtEnd(idx)
+            } else if x == word_start {
+                WordState::AtStart(idx)
+            } else {
+                WordState::InCenter(idx)
+            }
+        } else {
+            WordState::Out
+        }
+    }
+
     /// Moves to the previous word in the document
     pub fn move_prev_word(&mut self) -> Status {
         let Loc { x, y } = self.char_loc();
@@ -670,27 +702,49 @@ impl Document {
             return Status::StartOfLine;
         }
         // Find where all the words are
-        let mut searcher = Searcher::new(r"(^|\s{2,}|[A-Za-z0-9_]+|\.)");
-        let line = self
-            .line(y)
-            .unwrap_or_default()
-            .chars()
-            .take(x)
-            .collect::<String>();
-        let mut matches = searcher.rfinds(&line);
-        matches
-            .iter_mut()
-            .for_each(|m| m.loc.x += m.text.chars().count());
-        // Find the most appropriate one to move to given the cursor position
-        if let Some(mtch) = matches.first() {
-            if mtch.loc.x == x {
-                matches.remove(0);
+        let line = self.line(y).unwrap_or_default();
+        let words = self.word_boundaries(&line);
+        let state = self.cursor_word_state(&words, x);
+        // Work out where to move to
+        let new_x = match state {
+            // Go to start of line if at beginning
+            WordState::AtEnd(0) | WordState::InCenter(0) | WordState::AtStart(0) => 0,
+            // Cursor is at the middle / end of a word, move to previous end
+            WordState::AtEnd(idx) | WordState::InCenter(idx) => {
+                if let Some(word) = words.get(idx.saturating_sub(1)) {
+                    word.1
+                } else {
+                    // No previous word exists, just go to start of line
+                    0
+                }
             }
-        }
-        if let Some(mtch) = matches.first_mut() {
-            mtch.loc.y = self.loc().y;
-            self.move_to(&mtch.loc);
-        }
+            WordState::AtStart(idx) => {
+                // Cursor is at the start of a word, move to previous start
+                if let Some(word) = words.get(idx.saturating_sub(1)) {
+                    word.0
+                } else {
+                    // No previous word exists, just go to start of line
+                    0
+                }
+            }
+            WordState::Out => {
+                // Cursor is not touching any words, find previous end
+                let mut shift_back = x;
+                while let WordState::Out = self.cursor_word_state(&words, shift_back) {
+                    shift_back = shift_back.saturating_sub(1);
+                    if shift_back == 0 {
+                        break;
+                    }
+                }
+                match self.cursor_word_state(&words, shift_back) {
+                    WordState::AtEnd(idx) => words[idx].1,
+                    _ => 0,
+                }
+            }
+        };
+        // Perform the move
+        self.move_to_x(new_x);
+        // Clean up
         self.old_cursor = self.loc().x;
         Status::None
     }
@@ -704,12 +758,92 @@ impl Document {
             return Status::EndOfLine;
         }
         // Find and move to the next word
-        if let Some(mut mtch) = self.next_match(r"(\s{2,}|[A-Za-z0-9_]+|$|\.)", 0) {
-            mtch.loc.x += mtch.text.chars().count();
-            self.move_to(&mtch.loc);
-        }
+        let line = self.line(y).unwrap_or_default();
+        let words = self.word_boundaries(&line);
+        let state = self.cursor_word_state(&words, x);
+        // Work out where to move to
+        let new_x = match state {
+            // Cursor is at the middle / end of a word, move to next end
+            WordState::AtEnd(idx) | WordState::InCenter(idx) => {
+                if idx == words.len() {
+                    line.chars().count()
+                } else if let Some(word) = words.get(idx + 1) {
+                    word.1
+                } else {
+                    // No next word exists, just go to end of line
+                    line.chars().count()
+                }
+            }
+            WordState::AtStart(idx) => {
+                // Cursor is at the start of a word, move to next start
+                if idx == words.len() {
+                    line.chars().count()
+                } else if let Some(word) = words.get(idx + 1) {
+                    word.0
+                } else {
+                    // No next word exists, just go to end of line
+                    line.chars().count()
+                }
+            }
+            WordState::Out => {
+                // Cursor is not touching any words, find next start
+                let mut shift_forward = x;
+                while let WordState::Out = self.cursor_word_state(&words, shift_forward) {
+                    shift_forward += 1;
+                    if shift_forward == line.chars().count() {
+                        break;
+                    }
+                }
+                match self.cursor_word_state(&words, shift_forward) {
+                    WordState::AtStart(idx) => words[idx].0,
+                    _ => line.chars().count(),
+                }
+            }
+        };
+        // Perform the move
+        self.move_to_x(new_x);
+        // Clean up
         self.old_cursor = self.loc().x;
         Status::None
+    }
+
+    /// Function to delete a word at a certain location
+    /// # Errors
+    /// Errors if out of range
+    pub fn delete_word(&mut self) -> Result<()> {
+        let Loc { x, y } = self.char_loc();
+        let line = self.line(y).unwrap_or_default();
+        let words = self.word_boundaries(&line);
+        let state = self.cursor_word_state(&words, x);
+        let delete_upto = match state {
+            WordState::InCenter(idx) | WordState::AtEnd(idx) => {
+                // Delete back to start of this word
+                words[idx].0
+            }
+            WordState::AtStart(idx) => {
+                // Delete back to end of the previous word
+                if let Some(word) = words.get(idx.saturating_sub(1)) {
+                    word.1
+                } else {
+                    0
+                }
+            }
+            WordState::Out => {
+                // Delete back to the end of the previous word
+                let mut shift_back = x;
+                while let WordState::Out = self.cursor_word_state(&words, shift_back) {
+                    shift_back = shift_back.saturating_sub(1);
+                    if shift_back == 0 {
+                        break;
+                    }
+                }
+                match self.cursor_word_state(&words, shift_back) {
+                    WordState::AtEnd(idx) => words[idx].1,
+                    _ => 0,
+                }
+            }
+        };
+        self.delete(delete_upto..=x, y)
     }
 
     /// Function to search the document to find the next occurance of a regex
@@ -1209,4 +1343,12 @@ impl Document {
 pub struct Cursor {
     pub loc: Loc,
     pub selection_end: Loc,
+}
+
+/// State of a word
+pub enum WordState {
+    AtStart(usize),
+    AtEnd(usize),
+    InCenter(usize),
+    Out,
 }
